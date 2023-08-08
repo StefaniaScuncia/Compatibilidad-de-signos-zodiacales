f340c9e75b918b172d2f9aa0c10bbed89f1a8322
Stefania Scuncia <stefaniascuncia@gmail.com>
2023-08-07 22:31:40 -0300

    incorporacion de functions y mejora de switch

--- main.js
+++ main.js
@@ -1,73 +1,150 @@
+function saludoUsuario(nombre, nombrePareja) {
+  alert("Bienvenido/a " + nombre + ", continuemos para ver si vos y " + nombrePareja + " son compatibles" );
+}
+
+let ingreso = prompt("Ingresa tu nombre")
+let ingreso2= prompt ("Ingresa el nombre de tu pareja")
+
+saludoUsuario(ingreso, ingreso2)
 
 for (;;) {
   signoUsuario = prompt("Ingresa tu signo del zodiaco (en minúsculas):");
   if (!isNaN(signoUsuario)) {
     alert("Por favor, ingresa solo letras, sin números ni caracteres especiales.");
   } else {
-    break; // Salimos del bucle si la entrada es válida (no es un número)
+    break;
   }
 }
 
-// Pedimos al usuario que ingrese el signo del zodiaco de su pareja (en minúsculas) hasta que sea válido
+
 for (;;) {
-  signoPareja = prompt("Ingresa el signo del zodiaco de tu pareja (en minúsculas):");
+  signoPareja = prompt("Ingresa el signo del zodiaco de " + ingreso2 + " (en minúsculas):");
   if (!isNaN(signoPareja)) {
     alert("Por favor, ingresa solo letras, sin números ni caracteres especiales.");
   } else {
-    break; // Salimos del bucle si la entrada es válida (no es un número)
+    break;
   }
 }
 
-// Utilizamos un switch para determinar si los signos son compatibles
-switch (signoUsuario) {
-  case "aries":
-    switch (signoPareja) {
-      case "aries":
-      case "leo":
-      case "sagitario":
-        alert("Son compatibles.");
-        break;
-      default:
-        alert("No son compatibles.");
-    }
-    break;
-
-  case "tauro":
-    switch (signoPareja) {
-      case "tauro":
-      case "virgo":
-      case "capricornio":
-        alert("Son compatibles.");
-        break;
-      default:
-        alert("No son compatibles.");
-    }
-    break;
-
-    case "geminis":
-    switch (signoPareja) {
-      case "acuario":
-      case "virgo":
-      case "capricornio":
-        alert("Son compatibles.");
-        break;
-      default:
-        alert("No son compatibles.");
-    }
-    break;
-
+function verificarCompatibilidad(signoUsuario, signoPareja) {
+  switch (signoUsuario) {
+    case "capricornio":
+      switch (signoPareja) {
+        case "tauro":
+        case "virgo":
+        case "escorpio":
+          return true;
+        default:
+          return false;
+      }
+    case "leo":
+      switch (signoPareja) {
+        case "libra":
+        case "sagitario":
+        case "aries":
+          return true;
+        default:
+          return false;
+      }
+    case "cáncer":
+      switch (signoPareja) {
+        case "virgo":
+        case "escorpio":
+        case "tauro":
+          return true;
+        default:
+          return false;
+      }
+    case "aries":
+      switch (signoPareja) {
+        case "géminis":
+        case "acuario":
+        case "leo":
+        case "tauro":
+          return true;
+        default:
+          return false;
+      }
+    case "libra":
+      switch (signoPareja) {
+        case "leo":
+        case "sagitario":
+          return true;
+        default:
+          return false;
+      }
+    case "piscis":
+      switch (signoPareja) {
+        case "géminis":
+        case "sagitario":
+        case "leo":
+          return true;
+        default:
+          return false;
+      }
+    case "tauro":
+      switch (signoPareja) {
+        case "capricornio":
+        case "piscis":
+        case "virgo":
+          return true;
+        default:
+          return false;
+      }
+    case "virgo":
+      switch (signoPareja) {
+        case "cáncer":
+        case "tauro":
+        case "capricornio":
+          return true;
+        default:
+          return false;
+      }
+    case "géminis":
+      switch (signoPareja) {
+        case "acuario":
+        case "libra":
+          return true;
+        default:
+          return false;
+      }
     case "acuario":
-    switch (signoPareja) {
-      case "geminis":
-      case "virgo":
-      case "capricornio":
-        alert ("Son compatibles.");
-        break;
-      default:
-        alert("No son compatibles.");
-    }
-    break;
-  // Agrega más casos para otros signos del zodiaco según sea necesario
-  default:
-    alert("Signo del zodiaco no reconocido.");
+      switch (signoPareja) {
+        case "cáncer":
+        case "tauro":
+          return true;
+        default:
+          return false;
+      }
+    case "escorpio":
+      switch (signoPareja) {
+        case "cáncer":
+        case "piscis":
+        case "capricornio":
+          return true;
+        default:
+          return false;
+      }
+    case "sagitario":
+      switch (signoPareja) {
+        case "acuario":
+        case "libra":
+          return true;
+        default:
+          return false;
+      }
+    default:
+      return false;
+  }
+}
+
+// Ejemplo de uso:
+if (verificarCompatibilidad(signoUsuario, signoPareja)) {
+  alert("¡Felicidades, " + ingreso + ", vos y " + ingreso2 + " son compatibles!");
+} else {
+  alert(ingreso + ", lamentamos informarte que vos y " + ingreso2 + " no son compatibles.");
+}
+
+function mensaje (mensaje){
+  alert (mensaje)
 }
